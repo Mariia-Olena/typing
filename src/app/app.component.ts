a1d86513a7d5e449d51a810a753609db6183e27e
Mariia-Olena <mariia.stus@gmail.com>
2023-02-07 11:39:00 +0200

    created typing game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  text = lorem.sentence();
+  enteredText = '';
+
+  getValue(event: Event): string {
+    return (event.target as HTMLInputElement).value;
+  }
+
+  OnInput(value: string): void {
+    this.enteredText = value;
+  }
+
+  compare(enteredLetter: string, letter: string): string {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+
+    return enteredLetter === letter ? 'right' : 'wrong';
+  }
 }
